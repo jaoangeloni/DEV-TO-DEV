cc19048ffa0d8a2fe8f015fa196063e419d90b25
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-17 10:47:32 -0300

    PROJ

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <section>
+    <main className="bg-blue-800">
+      <section className=''>
 
       </section>
     </main>
