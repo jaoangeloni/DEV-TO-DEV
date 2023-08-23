2ca22e8bccba9d961bb60237575828963b53b349
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-23 08:36:10 -0300

    PROJ

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,9 @@ app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
-        resizable: false
+        resizable: false,
+        // frame: false
     });
 
-    mainWindow.loadURL('http://127.0.0.1:5500/dist/index.html');
+    mainWindow.loadURL('http://127.0.0.1:5500/dist/pages/index.html');
 });
---
+++ dist/scripts/index.js
@@ -0,0 +1,7 @@
+const images = [
+    "https://www.nme.com/wp-content/uploads/2020/11/Haven-Credit-The-Game-Bakers-HERO@2000x1270.jpg0",
+];
+
+const imagem = document.querySelector("#imagem");
+
+imagem.src = images[0]
