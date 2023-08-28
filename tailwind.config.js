21ed6afacf211af79dacc0241e4416d586a65320
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-28 09:07:15 -0300

    PROJ

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        'image0': "url('https://img.redbull.com/images/c_fill,w_1200,h_630,g_auto,f_auto,q_auto/redbullcom/2015/08/31/1331744414923_2/indie-games')",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
