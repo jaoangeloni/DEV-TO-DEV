369f076b9a379babf676d435d87bcdfd3a7e193f
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-30 08:04:15 -0300

    projota

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
@@ -10,9 +10,5 @@ app.on('ready', () => {
         frame: false
     });
 
-
-
     mainWindow.loadURL('http://127.0.0.1:5500/src/pages/index.html');
-    // mainWindow.loadURL('http://127.0.0.1:5500/src/pages/index.html');
-    // mainWindow.loadURL('http://127.0.0.1:5500/src/pages/index.html');
 });
