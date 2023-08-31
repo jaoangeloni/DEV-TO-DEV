750e3b4b6f001e1d5acc2b1aab555b8053536d3e
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-08-31 09:43:51 -0300

    PROJ

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
+
+//Gerador de imagens aleatórias quando abre o programa
 const carrossel = document.getElementById("carrossel");
-
-
 const images = [
     "../pages/assets/backgrounds/indie1.jpg",
     "../pages/assets/backgrounds/indie2.jpg",
@@ -14,3 +14,20 @@ carrossel.style.backgroundImage = `url('${images[aleatorio]}')`;
 setInterval(() => {
 }, 0);
 
+//Abrir modal de login e de registro de usuário
+const modalRegistrar = document.getElementById("registrar");
+const modalLogar = document.getElementById("logar");
+
+function goToCreate() {
+    modalLogar.classList.toggle("flex")
+    modalLogar.classList.toggle("hidden")
+    modalRegistrar.classList.toggle("hidden")
+    modalRegistrar.classList.toggle("flex")
+}
+
+function backToLogin() {
+    modalRegistrar.classList.toggle("flex")
+    modalRegistrar.classList.toggle("hidden")
+    modalLogar.classList.toggle("hidden")
+    modalLogar.classList.toggle("flex")
+}
