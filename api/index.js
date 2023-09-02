e3c650ebafa97fe0540a9cbd3e9dfd31fdeb30b4
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-09-02 12:14:44 -0300

    push

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
-const app = require('express');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("POSITIVO E OPERANTE");
+});
