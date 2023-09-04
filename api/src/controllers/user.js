4c775b41818a0a62e657fd24bbcb40eb8dee110c
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-09-04 11:34:21 -0300

    PROJ

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -9,9 +9,21 @@ const create = async (req, res) => {
         });
         return res.status(201).json(user).end();
     } catch (error) {
-        res.status(400).json({ error: error.message }).end();
+        res.status(206).json({ "msg": "Username ou Email já sendo utilizados" }).end();
     }
 }
+
+// const create = async (req, res) => {
+//     try {
+//         const data = req.body;
+//         const user = await prisma.user.create({
+//             data: data
+//         });
+//         return res.status(201).json(user).end();
+//     } catch (error) {
+//         res.status(400).json({ error: error.message }).end();
+//     }
+// }
 
 module.exports = {
     create
--- frontend/src/scripts/login.js
+++ frontend/src/scripts/login.js
@@ -2,36 +2,10 @@ const iUsername = document.getElementById("createUsername");
 const iEmail = document.getElementById("email");
 const iPassword = document.getElementById("createPassword");
 const iConfirmPassword = document.getElementById("confirmPassword");
+
 const modalError = document.getElementById("modalError");
 const modalIcon = document.getElementById("modalIcon");
 const warning = document.getElementById("warning");
-
-// function autenticar() {
-//     let data = {
-//         "userId": userId.value,
-//         "senha": senha.value
-//     }
-
-//     let options = {
-//         "method": "POST",
-//         "headers": {
-//             "Content-Type":"application/json"
-//         },
-//         "body": JSON.stringify(data)
-//     }
-
-//     fetch("http://localhost:3000/login", options)
-//     .then(resp => { return resp.json() })
-//     .then(info => {
-//         if(info.id != undefined) {
-//             localStorage.setItem("usuario", JSON.stringify(info));
-
-//             window.location.href="";
-//         }else {
-//             alert("Usuario ou Senha inválidos");
-//         }
-//     })
-// }
 
 function cadastrar() {
 
@@ -44,16 +18,9 @@ function cadastrar() {
         warning.innerHTML = "Senhas não conferem";
         modalError.classList.remove("opacity-0");
         modalError.classList.add("opacity-100");
-    }
-    else if (iPassword.value == iConfirmPassword.value && iPassword.value > 6) {
-        warning.innerHTML = "Cadastrado com sucesso";
-        modalIcon.src = './assets/right.png';
+    } else if (iPassword.value == iConfirmPassword.value && iPassword.value) {
         modalError.classList.remove("opacity-0");
         modalError.classList.add("opacity-100");
-        setTimeout(() => {
-            modalError.classList.remove("opacity-100");
-            modalError.classList.add("opacity-0");
-        }, 3000)
         let user = {
             "username": iUsername.value,
             "email": iEmail.value,
@@ -62,11 +29,24 @@ function cadastrar() {
         }
         api.post("/user", user)
             .then(resp => {
-                iUsername.value = ""
-                iEmail.value = ""
-                iPassword.value = ""
-                iConfirmPassword.value = ""
-            })
+                if (resp.status == 206) {
+                    warning.innerHTML = resp.data.msg;
+                    modalIcon.src = './assets/error.png';
+                } else {
+                    warning.innerHTML = "Cadastrado com sucesso";
+                    modalIcon.src = './assets/right.png';
+                    setTimeout(() => {
+                        modalError.classList.remove("opacity-100");
+                        modalError.classList.add("opacity-0");
+                    }, 5000);
+                    iUsername.value = "";
+                    iEmail.value = "";
+                    iPassword.value = "";
+                    iConfirmPassword.value = "";
+                    backToLogin();
+                }
+            }
+            )
     }
 
 }
