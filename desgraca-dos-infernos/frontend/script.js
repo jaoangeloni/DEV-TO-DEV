2af4f601e144fa6eda794c259b1ba10cfccabca8
Jao Angeloni <joaopedroangeloni@gmail.com>
2023-09-13 08:09:58 -0300

    desgraca

--- desgraca-dos-infernos/frontend/script.js
+++ desgraca-dos-infernos/frontend/script.js
@@ -1,6 +1,21 @@
 
 const url = 'http://localhost:3000/listar/1'
-fetch(url)
-.then(resp => {
-    console.log(resp)
-})
+const carregarImagemDoUsuario = async () => {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar a imagem do usuário');
+        }
+
+        const urlImage = await response.json();
+
+        // Selecionar a tag img pelo ID
+        const imgUsuario = document.getElementById('imagem');
+
+        // Definir o atributo 'src' da imagem com a URL obtida
+        imgUsuario.src = urlImage;
+    } catch (error) {
+        console.error(error);
+    }
+};
+carregarImagemDoUsuario();
--- desgraca-dos-infernos/prisma/migrations/20230913005150_data/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `User` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(191) NOT NULL,
-    `picture` LONGBLOB NOT NULL,
-
-    UNIQUE INDEX `User_username_key`(`username`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
