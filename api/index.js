c547a8f731ad9fd6bfcac133b7de9f43bd1f4e37
buzzfps <gabrielzontabatista@gmail.com>
2023-09-15 11:05:38 -0300

    proj

--- api/index.js
+++ api/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 
 const routeUsers = require('./src/routes/user');
+const routeComments = require('./src/routes/comments');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use(routeUsers);
+app.use(routeComments);
 
 
 app.listen(3000, () => {
