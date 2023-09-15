25b7c12e41750d16e46c826fb06ab75f5b611233
apphrodit <joaovrdf@gmail.com>
2023-09-15 10:40:43 -0300

    model

---
+++ api/src/models/gamePage.js
@@ -0,0 +1,9 @@
+class gamePage {
+  constructor(i) {
+    this.id = i.id;
+    this.idUser = i.idUser;
+    this.name = i.name;
+    this.gamePicture = i.gamePicture;
+    this.gameBanner = i.gameBanner;
+  }
+}
