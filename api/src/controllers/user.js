dfa2b7522a07270407a32c43d440a1b1e21beaf5
Joao Angeloni <joaopedroangeloni@gmail.com>
2023-10-18 11:12:43 -0300

    commit

--- api/src/controllers/user.js
+++ api/src/controllers/user.js
@@ -19,8 +19,12 @@ const criar = (req, res) => {
 
             // Conteúdo HTML para o perfil
             const perfilHTML = `
-            <html>
+            <!DOCTYPE html>
+            <html lang="en">
+
             <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>${username}</title>
                 <link rel="stylesheet" href="../../../dist/output.css">
                 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400&display=swap" rel="stylesheet">
