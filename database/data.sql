285aaa09d5293380381e103fbc656dee769bfeb1
Joao Angeloni <joaopedroangeloni@gmail.com>
2023-12-11 09:28:32 -0300

    commit

--- database/data.sql
+++ database/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO users(username, name, email, password, profilePicture, profileBanner) VALUES
-('johnsmith', 'John Smith', 'john@example.com', '12345', "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156380/userPFP/jkegbvvkxk8erjczei0b.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
-('emmajohnson', 'Emma Johnson', 'emma@example.com', '12345', "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156380/userPFP/f3zyn9lj8napedc0z4d5.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
-('michaeldavis', 'Michael Davis', 'michael@example.com', '12345', "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156380/userPFP/tvapbm9wrn0aptbzvibh.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
-('oliviabrown', 'Olivia Brown', 'olivia@example.com', '12345', "https://res.cloudinary.com/dneit0fsb/image/upload/v1698154247/userPFP/lgtnoe2gu1vd6ftx7tsh.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
-('williamtaylor', 'William Taylor', 'william@example.com', '12345', "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156381/userPFP/htg5hc2ythoo02rs1rmx.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg");
+('johnsmith', 'John Smith', 'john@example.com', PASSWORD('12345'), "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156380/userPFP/jkegbvvkxk8erjczei0b.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
+('emmajohnson', 'Emma Johnson', 'emma@example.com', PASSWORD('12345'), "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156380/userPFP/f3zyn9lj8napedc0z4d5.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
+('michaeldavis', 'Michael Davis', 'michael@example.com', PASSWORD('12345'), "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156380/userPFP/tvapbm9wrn0aptbzvibh.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
+('oliviabrown', 'Olivia Brown', 'olivia@example.com', PASSWORD('12345'), "https://res.cloudinary.com/dneit0fsb/image/upload/v1698154247/userPFP/lgtnoe2gu1vd6ftx7tsh.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg"),
+('williamtaylor', 'William Taylor', 'william@example.com', PASSWORD('12345'), "https://res.cloudinary.com/dneit0fsb/image/upload/v1698156381/userPFP/htg5hc2ythoo02rs1rmx.png", "https://res.cloudinary.com/dneit0fsb/image/upload/v1702296264/userPFP/bkd3v3rrnccmtpjwgxrv.jpg");
 
 INSERT INTO post (userId, descImage, postImage, mime_type) VALUES
 (1, "Alguma ideia de como fazer uma casa bonita?", "http://res.cloudinary.com/dneit0fsb/image/upload/v1702293738/ipbfry89t0mkyjqtqnhm.jpg", "image/jpeg"),
